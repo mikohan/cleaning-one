b6db3d7957889b4a5051b4fdfe7d6043956f9132
manhee <angara99@gmail.com>
2023-12-10 10:10:24 -0800

    Added video to page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import vidBg from '../assets/bmw_v3.mp4';
 
 export const Main = () => {
-  return <div>Some beautifull text</div>;
+  return (
+    <div className='main'>
+      <video src={vidBg} autoPlay loop muted />
+    </div>
+  );
 };
