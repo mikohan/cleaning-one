e80f893afac6aebbefa89a54e5c85b028b47c239
manhee <angara99@gmail.com>
2023-12-13 10:16:35 -0800

    Remove comment

--- src/components/CallToActionForm.tsx
+++ src/components/CallToActionForm.tsx
@@ -11,7 +11,6 @@ interface IProps {
 function BasicExample({ onSubmit }: IProps) {
   const [phoneValue, setPhoneValue] = React.useState<string>('');
   const [clientName, setClientName] = React.useState<string>('');
-  // Seems to be done
 
   const onSubmitForm = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
