b6b560e366029ab3c45410858b086a1ec1ba0bee
manhee <angara99@gmail.com>
2023-12-29 12:33:59 -0800

    Added pixel

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import './App.css';
 import { Main } from './components/Main';
 import MetaPixel from './utils/MetaPixel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactPixel from 'react-facebook-pixel';
 
 function App() {
   return (
     <div className='App'>
-      <MetaPixel />
       <Main />
     </div>
   );
