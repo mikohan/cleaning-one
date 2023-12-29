88f6431bb6a269048c8aa18e007cf57854d26cb0
manhee <angara99@gmail.com>
2023-12-29 12:13:18 -0800

    Installed fb pixel

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Main } from './components/Main';
+import MetaPixel from './utils/MetaPixel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className='App'>
+      <MetaPixel />
       <Main />
     </div>
   );
